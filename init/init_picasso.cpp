614766799c06985db79c5ac72e6a514d7a6c3e1a
MangoTM <mango@gmail.com>
2021-03-10 23:29:52 +0200

    picasso:switch to Marach coral FP

--- init/init_picasso.cpp
+++ init/init_picasso.cpp
@@ -66,11 +66,11 @@ constexpr const char *DEVICES[] = {
 };
 
 constexpr const char *BUILD_DESCRIPTION[] = {
-    "coral-user 11 RQ1A.210105.003 7005429 release-keys",
+    "coral-user 11 RQ2A.210305.006 7119741 release-keys",
 };
 
 constexpr const char *BUILD_FINGERPRINT[] = {
-    "google/coral/coral:11/RQ1A.210105.003/7005429:user/"
+    "google/coral/coral:11/RQ2A.210305.006/7119741:user/"
     "release-keys",
 };
 
